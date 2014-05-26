3aea30aba2a88e9cae4d079d28b684f553721bcd
Jeremy ETEC Lab <ruhlanj@students.wwu.edu>
2014-05-26 04:00:52 -0700

    removed unneeded spi function

--- firmware/spi.h
+++ firmware/spi.h
@@ -6,11 +6,6 @@
 * (void) spiInit(void)                  Function initializes SPI system into  *
 *                                       0,0 (clock idles low), LSB first      *
 *                                       master mode at 1Mhz.                  *
-* (void) spiPeripheralSelect(uint8_t)   Function activates proper chip select *
-*                                       line for peripheral port. Accepts     *
-*                                       8-bit value ranging from 1-4          *
-*                                       (0x01-0x04). Out of range argument    *
-*                                       will disable all peripherals.         *
 * (uint8_t) spiByte(uint8_t)            Function exchanges byte, returns      *
 *                                       value left in SPI Rx buffer. Blocks   *
 *                                       until Tx complete.                    *
