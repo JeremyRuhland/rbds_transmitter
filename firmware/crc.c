a5f5d3eb23001bbbfe034e7e4eb8559c3955e757
Jeremy ETEC Lab <ruhlanj@students.wwu.edu>
2014-05-28 04:42:43 -0700

    crc is probably wrong still

--- firmware/crc.c
+++ firmware/crc.c
@@ -1,10 +1,17 @@
 #include "includes.h"
 
-const uint8_t crcOffsetTable[6] PROGMEM = {0x3f, 0x66, 0x5a, 0xd4, 0x6d, 0x00};
+static const uint16_t crcOffsetTable[6] PROGMEM = {0x00fc, 0x0198, 0x0168, 0x0350, 0x01b4, 0x0000};
+
+// g(x) = x^10 + x^8 + x^7 + x^5 + x^4 + x^3 + 1
+#define CRCPOLY ((uint16_t) 0x05b9)
 
 uint16_t crcChecksum(rbds_t *rbds, uint8_t offset) {
-    uint16_t crcReturnValue = 0;
-    
-    crcReturnValue += (((uint16_t) pgm_read_byte(&crcOffsetTable[offset]))<<2);
+    uint16_t crcReturnValue;
+
+    crcReturnValue = ((uint16_t) (*rbds.hex >> 16)); // Get message from rbds packet
+    crcReturnValue = ((uint16_t) (crcReturnValue * 0x200));
+    crcReturnValue = (crcReturnValue + (crcReturnValue ^ CRCPOLY));
+    crcReturnValue += pgm_read_word(&crcOffsetTable[offset]); // Add group offset
+    crcReturnValue &= ~(0xfc00); // Strip off excess high bites
     return (crcReturnValue);
 }
